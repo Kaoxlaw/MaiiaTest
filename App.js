8d97a37ddd669a3fdf2d520348384da2c7ecc173
Mickael <ly.mickael@gmail.com>
2021-10-20 15:06:28 +0200

    Fetch data, Sort and list 10 by 10 working great

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import { myStyles } from "./styles/style";
 
-export default function App() {
+const dataLink = "https://jsonplaceholder.typicode.com/posts";
+
+const App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(dataLink)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => alert(error))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <SafeAreaView style={myStyles.container}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <View>
+          <FlatList
+            data={data.sort((a, b) => a.title.localeCompare(b.title))}
+            keyExtractor={({ id }) => id.toString()}
+            renderItem={({ item }) => (
+              <Text style={myStyles.title}>{item.id} {item.title}</Text>
+            )}
+            maxToRenderPerBatch={10}
+            initialNumToRender={10}
+            windowSize={9}
+          />
+        </View>
+      )}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ styles/style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const myStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: 70,
+    paddingHorizontal: 20,
+    backgroundColor: "#eaeaea",
+  },
+  title: {
+    fontSize: 18,
+  },
+});
